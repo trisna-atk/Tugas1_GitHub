dd30e1549ebed5f3416a3f8464e752d624e3e228
Trisna Atika Darmawati <114461291+trisna-atk@users.noreply.github.com>
2025-03-03 11:12:17 +0700

    Add files via upload

---
+++ hello.dart
@@ -0,0 +1,26 @@
+void main() {
+  print('Hello World');
+
+  //deklarasi menggunakan tipe data
+  String nama = "Trisna"; //tipe data string
+  int umur = 22; //tipe data integer
+  print(nama);
+  print(umur);
+
+  //deklarasi menggunakan var
+  var name = "John";
+  print(name);
+
+  //deklarasi menggunakan final
+  final lastName = "Doe";
+  print(lastName);
+
+  //deklarasi menggunakan const
+  const PI = 3.14;
+  print(PI);
+
+  //deklarasi menggunakan let
+  late String address;
+  address = 'Jl. ABC';
+  print(address);
+}
